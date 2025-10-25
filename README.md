1a7cfaa335f70394490e916c61632bcc3f84c026
XavierDupuis <github-xavierdupuis@outlook.com>
2025-10-24 23:28:38 -0400

    fix sudo

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 ## One liner install
 
-### With sudo
 ```sh
-sudo apt update -y && sudo apt install curl -y && sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/XavierDupuis/dotfiles/refs/heads/main/setup.sh)"
-```
-
-### Without sudo
-```sh
-apt update -y && apt install curl -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/XavierDupuis/dotfiles/refs/heads/main/setup.sh)"
+sudo apt update -y && sudo apt install curl -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/XavierDupuis/dotfiles/refs/heads/main/setup.sh)"
 ```
--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-apt update -y
-apt install curl zsh git fzf stow -y
+sudo apt update -y
+sudo apt install curl zsh git fzf stow -y
 
 git clone https://github.com/XavierDupuis/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 stow .
-cd ~
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
 chsh -s $(which zsh)
 zsh
