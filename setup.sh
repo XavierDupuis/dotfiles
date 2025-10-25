3c4450fe024b6493b4080a638e1e1cc1ab8a409a
XavierDupuis <github-xavierdupuis@outlook.com>
2025-10-24 23:32:09 -0400

    cd back to previous dir

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt install curl zsh git fzf stow -y
 git clone https://github.com/XavierDupuis/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 stow .
-cd ~
+cd -
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
