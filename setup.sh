a05c1e417fd3f9fc7e4f74dab63257d3cefd93df
XavierDupuis <github-xavierdupuis@outlook.com>
2025-10-24 22:28:55 -0400

    clone and stow

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 apt install zsh git fzf stow -y
+git clone https://github.com/XavierDupuis/dotfiles.git ~
+cd ~/dotfiles
+stow .
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
