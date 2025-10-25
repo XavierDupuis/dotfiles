72854aa5f1263428403b2939ff5848c0b6780333
XavierDupuis <github-xavierdupuis@outlook.com>
2025-10-24 22:41:01 -0400

    unattended

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/XavierDupuis/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 stow .
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
