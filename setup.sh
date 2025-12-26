d3f5f0c4b7ae3921fca4c8d25bcaf9cc2d7ca2f4
XavierDupuis <github-xavierdupuis@outlook.com>
2025-12-26 17:40:54 -0500

    extract target directory to var

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
+DOTFILES_DIRECTORY_PATH=~/.dotfiles
+
 sudo apt update -y
 sudo apt install curl zsh git fzf stow -y
 
-git clone https://github.com/XavierDupuis/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
+git clone https://github.com/XavierDupuis/dotfiles.git $DOTFILES_DIRECTORY_PATH
+cd $DOTFILES_DIRECTORY_PATH
 stow .
 cd -
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 chsh -s $(which zsh)
